172853e24624f879bb606b59610806a61037f186
Joe Brown <sparkyjoe94@aim.com>
2017-09-06 11:33:52 -0700

    initial

---
+++ index.ts
@@ -0,0 +1,8 @@
+export default function isWindows() {
+  if (typeof process === 'undefined' || !process) {
+    return false;
+  }
+  return process.platform === 'win32' ||
+  process.env.OSTYPE === 'cygwin' ||
+  process.env.OSTYPE === 'msys';
+};
